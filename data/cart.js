c25e02d296824f548e29a9fd3e9607e3885b8a7e
Ishan Voleti <ishan.voleti@gmail.com>
2025-05-01 15:48:32 -0400

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,13 @@
 //can be used outside of cart.js
-export const cart = [];
+export const cart = [{
+  //Normalizing the data -> we are only using the productID and accessing the product from products.js (prevents duplication)
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+},
+{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
 
 export function addToCart(productId) {
   let matchingItem;
